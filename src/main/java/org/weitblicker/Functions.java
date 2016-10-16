6434fa454ecdaf2f0706ee110a0685b8b8d15b92
whiterabbit <nww@posteo.de>
2016-10-16 15:31:31 +0200

    ObjectConverter and Functions dummy

---
+++ src/main/java/org/weitblicker/Functions.java
@@ -0,0 +1,9 @@
+package org.weitblicker;
+
+/**
+ * Created by nizzke on 16.10.16.
+ * @Purpose: Functions for the main program
+ */
+
+public class Functions {
+}
---
+++ src/main/java/org/weitblicker/ObjectConverter.java
@@ -0,0 +1,10 @@
+package org.weitblicker;
+
+/**
+ * Created by nizzke on 16.10.16.
+ * @Purpose: Convert Database Objects to export them as json for the app
+ */
+
+public class ObjectConverter {
+}
+
