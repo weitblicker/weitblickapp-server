da4d3c04c5cd03ebc3e0dd4458869b738965b48f
nizzke <nww@posteo.de>
2016-10-25 09:07:06 +0200

    Campaign merge

--- src/main/java/org/weitblicker/database/Campaign.java
+++ src/main/java/org/weitblicker/database/Campaign.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.Date;
-
 
 // TODO: 18.10.16 responsible (class + helper): nizzke
 
