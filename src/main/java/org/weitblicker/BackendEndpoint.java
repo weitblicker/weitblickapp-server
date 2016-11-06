6139f3a249dbc31fc3a038f03780c0db2f1f17b8
Sebstian Pütz <spuetz@uos.de>
2016-11-06 19:07:08 +0100

    fixed file as response

--- src/main/java/org/weitblicker/BackendEndpoint.java
+++ src/main/java/org/weitblicker/BackendEndpoint.java
@@ -1,10 +1,14 @@
 package org.weitblicker;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,6 +16,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.apache.tika.Tika;
 
 @Path("backend/")
@@ -25,16 +31,25 @@ public class BackendEndpoint {
 	}
 	
 	@GET
-	@Path("download/{file}")
-	@Consumes("text/plain; charset=UTF-8")
+	@Path("download/{file:.*}")
+	@Consumes("application/octet-stream")
 	public Response getAttachment(@PathParam("file") String fileName) {
+		
 		if (fileName == null) {
 			System.err.println("No such item");
 				return Response.status(Response.Status.BAD_REQUEST).build();
 		}
 		
-		// TODO ensure that the file is inside the correct folder to prevent hacker attacks!
-		File file = new File("webfiles/"+ fileName);
+		if (fileName.contains("..")){
+			// illegal folder traversal 
+			Response.status(Status.BAD_REQUEST).build();
+		}
+		
+		// TODO ensure that the file is inside 
+		// the correct folder to prevent hacker attacks!
+		// TODO get path to resource folder!
+
+		File file = new File("files/"+ fileName);
 
 		if(file.exists()){
 			return Response.ok(file, MediaType.APPLICATION_OCTET_STREAM).build();
@@ -43,29 +58,35 @@ public class BackendEndpoint {
 	}
 	
 	@GET
-	@Path("file/{file}")
-	@Produces("text/html")
+	@Path("file/{file:.*}")
+	@Produces("*/*")
 	public Response getFile(@PathParam("file") String fileName){
 		if (fileName == null) {
 			System.err.println("No such item");
 				return Response.status(Response.Status.BAD_REQUEST).build();
 		}
 		
-		// TODO ensure that the file is inside the correct folder to prevent hacker attacks!
+		if (fileName.contains("..")){
+			// illegal folder traversal 
+			Response.status(Status.BAD_REQUEST).build();
+		}
+		
+		// TODO ensure that the file is inside
+		// the correct folder to prevent hacker attacks!
 		// TODO get path to resource folder!
 		File file = new File("files/"+ fileName);
 		System.out.println(file.getAbsolutePath());
 
+		if(!file.exists()){
+			System.out.println("file does not exist!");
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		
 		try {
-			InputStream inputStream = new FileInputStream(file);
-			
 			Tika tika = new Tika();
-			String mimeType = tika.detect(inputStream);
-			System.out.println("MimeType: " + mimeType);
-			return Response.ok(inputStream, mimeType).build();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			String mimeType = tika.detect(file);
+			return Response.ok(file, mimeType).build();
+		} catch (Exception e){
 			e.printStackTrace();
 		}
 		return Response.status(Response.Status.BAD_REQUEST).build();
